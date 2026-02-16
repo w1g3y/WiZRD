d4916463548390127065d9dcaa02d85e59bf1ee5
w1g3y <nigel.weeks@gmail.com>
2026-02-16 16:53:50 +1100

    Delete WiZRD_client.sh

--- WiZRD_client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "select 'wzrd_task_' || int_queueid || '.wcmd', str_cmd from getNextSpell('${1}', '${2}');"  | isql-fb -u sysdba -p masterkey localhost:/var/db/firebird/wizrd.fdb | grep "wzrd_task" | awk '{print "echo "$2" > "$1" && chmod +x "$1" && ./"$1" >> "$1".log"}'
-
